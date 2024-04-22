5e2fd824c056feb4f635cbbd47763615f7e4a853
zombiehunter <viktormallory25@gmail.com>
2024-04-22 20:21:53 +0300

    minor fixes

--- hw04/commands.sh
+++ hw04/commands.sh
@@ -6,5 +6,4 @@ docker build -t notes .
 docker run -v $(pwd)/app_data/:/notes_app/data/ -it --rm --name notes_app notes python3 notes_app.py add -t "Feed the cat"
 docker run -v $(pwd)/app_data/:/notes_app/data/ -it --rm --name notes_app notes python3 notes_app.py add -t "Do the chores"
 docker run -v $(pwd)/app_data/:/notes_app/data/ -it --rm --name notes_app notes python3 notes_app.py add -t "test" -m "123"
-docker run -v $(pwd)/app_data/:/notes_app/data/ -it --rm --name notes_app notes python3 notes_app.py add -t "test" -m "123"
 docker run -v $(pwd)/app_data/:/notes_app/data/ -it --rm --name notes_app notes python3 notes_app.py list
